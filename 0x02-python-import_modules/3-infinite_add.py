402066151bf8894a03fe64fbab8fab438af478dc
zoubjd <zouhairbajdouri19@gmail.com>
2024-03-15 02:33:16 +0000

    push

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    import sys
+    
+    result = 0
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0")
+    else:
+        for i in range(count):
+            result += int(sys.argv[i + 1])
+        print("{}".format(result))
