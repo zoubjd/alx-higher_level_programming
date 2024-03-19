515689ca8b110d3d33419b58793faea283ca48dd
zoubjd <zouhairbajdouri19@gmail.com>
2024-03-19 00:55:45 +0000

    hh

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if not sentence:
+        sentence = None
     if sentence:
-        return len(sentence), sentence[0]
+        lenght = len(sentence)
     else:
-        return None
+        lenght = 0
+    return (lenght, sentence if not sentence else sentence[:1])
