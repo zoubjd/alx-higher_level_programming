117dcfd8d7003cf72bd383771fa7f6b6046d0b85
zoubjd <zouhairbajdouri19@gmail.com>
2024-03-22 06:41:01 +0000

    kk

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,10 @@ if number < 0:
     number = -number
     print(f"-{number % 10} and is less than 6 and not 0")
 
-if (number % 10) > 5:
-    print(f"{number % 10} and is greater than 5")
-elif (number % 10) == 0:
-    print(f"{number % 10} and is 0")
-elif (number % 10) < 6 and (number % 10) != 0:
-    print(f"{number % 10} and is less than 6 and not 0")
+else:
+    if (number % 10) > 5:
+        print(f"{number % 10} and is greater than 5")
+    elif (number % 10) == 0:
+        print(f"{number % 10} and is 0")
+    elif (number % 10) < 6 and (number % 10) != 0:
+        print(f"{number % 10} and is less than 6 and not 0")
