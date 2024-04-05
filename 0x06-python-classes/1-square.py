d74fb95688091dcea4a2963d2fe07bfe2be554f8
zoubjd <zouhairbajdouri19@gmail.com>
2024-04-05 06:40:35 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a Square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
