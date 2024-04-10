81e54386b031db546625ea1cebb7011c838bd84c
zoubjd <zouhairbajdouri19@gmail.com>
2024-04-10 05:45:27 +0000

    4 5

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -14,7 +14,6 @@ def print_square(size):
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
     if size < 0:
         raise ValueError("size must be >= 0")
 
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,7 +13,6 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
     for delim in ".?:":
         # print(delim, text.split(delim))
         text = (delim + "\n\n").join(
