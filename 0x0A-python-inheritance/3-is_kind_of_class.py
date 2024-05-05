44d08ee00bd6d72542383c392a88cd96fdbb958a
zoubjd <zouhairbajdouri19@gmail.com>
2024-05-05 01:08:03 +0100

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""CHEcks for weither """
+
+
+def is_kind_of_class(obj, a_class):
+    """returns true if obj and a_classe are the same
+    Args:
+    a: the object
+    a_class: the class to be tested
+
+    Return: True or False
+    """
+
+    return isinstance(obj, a_class)
