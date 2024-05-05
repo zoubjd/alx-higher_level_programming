dc20d1d0f3160ae838917c69e26348e23273a68f
zoubjd <zouhairbajdouri19@gmail.com>
2024-05-05 03:04:06 +0100

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a module for Rectang"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """a class named Square"""
+
+    def __init__(self, size):
+        """square instatiation"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """a methode"""
+        return self.__size * self.__size
