a2ae9af4508cc8b92383d895885cf5d4e4fde255
zoubjd <zouhairbajdouri19@gmail.com>
2024-05-05 03:08:34 +0100

    all mandatory done

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a module for Rectang"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a class named Square"""
+
+    def __init__(self, size):
+        """square instatiation"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """a methode"""
+        return self.__size * self.__size
+    
+    def __str__(self):
+        """string representation methode"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
+
