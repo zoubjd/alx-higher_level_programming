1de313bdecaf3a349bb872a08af26db3be0a63c0
zoubjd <zouhairbajdouri19@gmail.com>
2024-06-19 01:32:57 +0100

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,8 +4,8 @@ if (process.argv[2] === undefined) {
   console.log('Not a number');
 } else if (process.argv[2] !== undefined) {
   if (parseInt(process.argv[2])) {
-      console.log(parseInt(process.argv[2]));
-    } else {
-        console.log('Not a number');
-    }
+    console.log(parseInt(process.argv[2]));
+  } else {
+    console.log('Not a number');
+  }
 }
