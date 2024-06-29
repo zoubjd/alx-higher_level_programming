1a8772c85616beb1e04317980ff4ddc9b5eb205a
zoubjd <zouhairbajdouri19@gmail.com>
2024-06-29 04:23:46 +0100

    first push

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+mysql_usr = sys.argv[1]
+mysql_pwd = sys.argv[2]
+mysql_db = sys.argv[3]
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_usr, passwd=mysql_pwd, db=mysql_db)
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+the first python and mysql directory
