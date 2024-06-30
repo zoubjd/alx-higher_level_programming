a26ecb53b53db195191b4dbee6454c60223f6e67
zoubjd <zouhairbajdouri19@gmail.com>
2024-06-30 19:58:16 +0100

    ato

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""the first mysqlpython module"""
-import sys
+"""  list all states from the database """
+from sys import argv
 import MySQLdb
 
-mysql_usr = sys.argv[1]
-mysql_pwd = sys.argv[2]
-mysql_db = sys.argv[3]
+if __name__ == '__main__':
 
-conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_usr, passwd=mysql_pwd, db=mysql_db)
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    mysql_usr = argv[1]
+    mysql_pwd = argv[2]
+    mysql_db = argv[3]
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_usr, passwd=mysql_pwd, db=mysql_db)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
