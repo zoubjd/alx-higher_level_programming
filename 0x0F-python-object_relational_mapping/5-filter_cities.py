4150718a085800cf76a3acd397ef1de6f4689430
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-01 17:34:39 +0100

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" lists all cities from the database with a given state """
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    mysql_usr = argv[1]
+    mysql_pwd = argv[2]
+    mysql_db = argv[3]
+    state = argv[4]
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=mysql_usr,
+                           passwd=mysql_pwd,
+                           db=mysql_db)
+    cursor = conn.cursor()
+    cursor.execute("SELECT cities.name\
+                    FROM cities\
+                    INNER JOIN states ON cities.state_id = states.id\
+                    WHERE BINARY states.name = '{}'\
+                    ORDER BY cities.id ASC".format(state))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
