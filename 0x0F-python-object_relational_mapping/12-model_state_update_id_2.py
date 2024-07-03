b086396bc7ce26ffe53415e94df47bc8f11eb477
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-03 05:42:24 +0100

    push

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""   changes the name of a State object from the database """
+
+from sys import argv
+from sqlalchemy import create_engine, alter, where
+from sqlalchemy.orm.session import sessionmaker, Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    password = argv[1]
+    username = argv[2]
+    database = argv[3]
+    state = "New Mexico"
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(State(name=state))
+
+    state = session.query(State).alter(name=state).where(State.id == 2)
+    print('{}'.format(state.id))
+
+    session.commit()
