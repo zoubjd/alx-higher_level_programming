d46e3e874e7c3731cece2acc225d5c1e3bbd87af
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-05 23:25:14 +0100

    push

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """sends a post request and displays the value"""
-from urllib import parse, request
+import urllib.request
 from sys import argv
 
 
 if __name__ == "__main__":
     url = argv[1]
-    email = argv[2]
-    values = {'email': email}
+    email = {'email': argv[2]}
 
-    data = parse.urlencode(values)
+    data = urllib.parse.urlencode(email)
     data = data.encode('utf-8')
 
-    """with request.urlopen(url, data) as response:
-        html = response.read()
-        print(html.decode('utf-8'))"""
-    with request.Request(url, data) as response:
-        resp = request.urlopen(response)
-        respdata = resp.read().decode('utf-8')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(url, data) as response:
+        respdata = response.read()
+        respdata = respdata.decode('utf-8')
         print(respdata)
