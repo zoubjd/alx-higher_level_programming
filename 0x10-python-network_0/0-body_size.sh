30266ef33b6562883f2c92b2b516e0e89d03fcc3
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-05 03:13:27 +0100

    push

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# sends a request to that URL, and displays the size
+
+URL = #1
+curl -sI $URL | grep -i Content-Length | awk '{print $2}'
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,1 @@
+this is a python and web directory
