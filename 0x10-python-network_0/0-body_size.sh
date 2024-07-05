ad35a10cb5ddac5af686f4f6bf7a4cff40888ce6
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-05 03:32:10 +0100

    push

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size
-URL="$1"
-curl -sI $URL | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Make request, display response
+curl -sX DELETE "$1"
