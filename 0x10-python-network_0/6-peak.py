3f65e7d49ad15bf20a734833fdf15e945ee988fe
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-05 03:58:48 +0100

    push

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""finds the peak """
+
+def find_peak(list_of_integers):
+    """finds the peak"""
+    if list_of_integers == []:
+        return
+    max = list_of_integers[0]
+    for i in list_of_integers:
+        if i > max:
+            max = i
+    return max
