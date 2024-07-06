ada7b0555fd8b986b895213b09ff476325932b64
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-06 03:54:35 +0100

    push

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""list 10 commits"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    r = requests.get(url)
+    commits = r.json()
+    for i in range(10):
+        sha = commits[i].get('sha')
+        author = commits[i].get('commit').get('author').get('name')
+        print('{}: {}'.format(sha, author))
