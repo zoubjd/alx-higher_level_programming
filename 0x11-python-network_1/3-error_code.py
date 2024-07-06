2f781a74ba71e21f0e3e334ac0352b054f7559aa
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-06 02:59:46 +0100

    push

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,9 +8,8 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
 
-    with urllib.request.Request(url) as response:
-        try:
-            with urllib.request.urlopen(response) as response:
-                print(response.read().decode('utf-8'))
-        except urllib.error.HTTPError as e:
-            print('Error code: {}'.format(e.code))
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
