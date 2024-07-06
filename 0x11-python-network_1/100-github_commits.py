30fc5761c28d64ecab525c5530e2fb6d88b3595f
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-06 19:19:23 +0100

    push

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,16 +5,13 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    url = 'https://api.github.com/repos/{}/{}/\
+    commits?per_page=10'.format(argv[2], argv[1])
+
     r = requests.get(url)
     commits = r.json()
-    if len(commits) < 10:
-        for i in len(commits):
-            sha = commits[i].get('sha')
-            author = commits[i].get('commit').get('author').get('name')
-            print('{}: {}'.format(sha, author))
-    else:
-        for i in range(10):
-            sha = commits[i].get('sha')
-            author = commits[i].get('commit').get('author').get('name')
-            print('{}: {}'.format(sha, author))
+
+    for commit in commits:
+        sha = commit.get('sha')
+        author = commit.get('commit').get('author').get('name')
+        print('{}: {}'.format(sha, author))
