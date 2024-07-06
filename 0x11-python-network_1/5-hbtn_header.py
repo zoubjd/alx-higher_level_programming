0899293f546c0817e5e3db0fff9bf465ef540343
zoubjd <zouhairbajdouri19@gmail.com>
2024-07-06 03:16:17 +0100

    push

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,6 +6,5 @@ from sys import argv
 
 if __name__ == "__main__":
     response = requests.get(argv[1])
-    html = response.read()
     if 'X-Request-Id' in response.headers:
         print(response.headers['X-Request-Id'])
